cc44208a33a69d4f3a11e97bae4e92ce4697d428
hqvnd <cccvvglhkim1@gmail.com>
2025-04-27 13:30:29 +0500

    bst and bsttest

--- src/BST.java
+++ src/BST.java
@@ -1,3 +1,5 @@
+import java.util.Iterator;
+import java.util.NoSuchElementException;
 import java.util.Stack;
 
 public class BST<K extends Comparable<K>, V> {
@@ -18,5 +20,159 @@ public class BST<K extends Comparable<K>, V> {
     public BST() {
         root = null;
         size = 0;
+    }
+
+    public void put(K key, V val) {
+        root = put(root, key, val);
+    }
+
+    private Node put(Node node, K key, V val) {
+        if (node == null) {
+            size++; // increment size when adding a new node
+            return new Node(key, val);
+        }
+
+        int cmp = key.compareTo(node.key);
+        if (cmp < 0) {
+            node.left = put(node.left, key, val);
+        } else if (cmp > 0) {
+            node.right = put(node.right, key, val);
+        } else {
+            node.val = val; // update value if key already exists
+        }
+
+        return node;
+    }
+
+    public V get(K key) {
+        Node node = get(root, key);
+        return node == null ? null : node.val;
+    }
+
+    private Node get(Node node, K key) {
+        if (node == null) {
+            return null;
+        }
+
+        int cmp = key.compareTo(node.key);
+        if (cmp < 0) {
+            return get(node.left, key);
+        } else if (cmp > 0) {
+            return get(node.right, key);
+        } else {
+            return node;
+        }
+    }
+
+    public void delete(K key) {
+        root = delete(root, key);
+    }
+
+    private Node delete(Node node, K key) {
+        if (node == null) return null;
+
+        int cmp = key.compareTo(node.key);
+        if (cmp < 0) {
+            node.left = delete(node.left, key);
+        } else if (cmp > 0) {
+            node.right = delete(node.right, key);
+        } else {
+            // only one child or no child
+            if (node.left == null) {
+                size--;
+                return node.right;
+            } else if (node.right == null) {
+                size--;
+                return node.left;
+            }
+
+            // two children
+            Node temp = node;
+            node = min(temp.right); // Find successor
+            node.right = deleteMin(temp.right);
+            node.left = temp.left;
+        }
+
+        return node;
+    }
+
+    private Node min(Node node) {
+        if (node.left == null) return node;
+        return min(node.left);
+    }
+
+    private Node deleteMin(Node node) {
+        if (node.left == null) {
+            size--;
+            return node.right;
+        }
+        node.left = deleteMin(node.left);
+        return node;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public class Entry {
+        private K key;
+        private V value;
+
+        public Entry(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        public K getKey() {
+            return key;
+        }
+
+        public V getValue() {
+            return value;
+        }
+
+        @Override
+        public String toString() {
+            return "{" + key + " : " + value + "}";
+        }
+    }
+
+    public Iterable<Entry> iterator() {
+        return new Iterable<Entry>() {
+            @Override
+            public Iterator<Entry> iterator() {
+                return new InOrderIterator();
+            }
+        };
+    }
+
+    private class InOrderIterator implements Iterator<Entry> {
+        private Stack<Node> stack = new Stack<>();
+
+        public InOrderIterator() {
+            pushLeft(root);
+        }
+
+        private void pushLeft(Node node) {
+            while (node != null) {
+                stack.push(node);
+                node = node.left;
+            }
+        }
+
+        @Override
+        public boolean hasNext() {
+            return !stack.isEmpty();
+        }
+
+        @Override
+        public Entry next() {
+            if (!hasNext()) throw new NoSuchElementException();
+
+            Node node = stack.pop();
+            pushLeft(node.right);
+
+            return new Entry(node.key, node.val);
+        }
     }
 }
--- src/BSTTest.java
+++ src/BSTTest.java
@@ -1,3 +1,36 @@
 public class BSTTest {
+    public static void main(String[] args) {
+        BST<Integer, String> tree = new BST<>();
 
+        // the elements
+        tree.put(50, "Root");
+        tree.put(30, "Left");
+        tree.put(70, "Right");
+        tree.put(20, "Left.Left");
+        tree.put(40, "Left.Right");
+        tree.put(60, "Right.Left");
+        tree.put(80, "Right.Right");
+
+        System.out.println("size: " + tree.size());
+        //...
+        System.out.println("val(40): " + tree.get(40));
+        System.out.println("val(90): " + tree.get(90));
+
+        // Test in-order traversal
+        System.out.println("in-order traversal:");
+        for (var elem : tree.iterator()) {
+            System.out.println("key is " + elem.getKey() + " and value is " + elem.getValue());
+        }
+
+//        // delete
+//        System.out.println("\nafter deleting key 30:");
+//        tree.delete(30);
+//        System.out.println("size: " + tree.size());
+//
+//        // Test in-order traversal after deletion
+//        System.out.println("in-order traversal after deletion:");
+//        for (var elem : tree.iterator()) {
+//            System.out.println("key is " + elem.getKey() + " and value is " + elem.getValue());
+//        }
+    }
 }
